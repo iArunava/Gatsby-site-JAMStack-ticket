7bdfb6c6d65b0a78daecf2b5064cd8fdaaa4adbd
Arunava <learningdroidarunava@gmail.com>
2018-08-29 01:43:41 +0530

    Inital commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p> Hi, I am Arunava,
+a student who is highly interested in AI and just loves to build AI products that can be used for the betterment of humanity (and in general). I currently am catching up with the fast-moving field of AI. Saying, I am interested in Deep Learning, Machine Learning, Reinforcement Learning, Computer Vision and NLP.
+My latest personal project that uses AI (learning algorithms), is iSeeDigits, which uses a model trained from scratch and uses that to predict handwritten digits on the web.
+
+I am also interested in building Web Applications and Andriod Applications and am highly skilled at Web Development and moderately skilled in Android Development. I have built quite a handful of applications that shows my Web Development and Android Development Skills. To name a few, one of my latest WebApp is Blemger, which allows one to blends two images, my last Android App has been developed a while ago, its called, Learn A Word, which was built as a part of Oxford API competition 2017.
+
+If you are interested more in the projects I have been working on or has worked on, be sure to check the projects section of my personal site (under development)
+
+Coming on to programming challenges, I just love competitive programming, so you can find me on all Competitive Programming sites, namely, CodeForces, CodeChef, UVa and so on.
+To track all my submissions, feel free to check out my StopStalk profile.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
